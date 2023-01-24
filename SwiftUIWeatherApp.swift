9eba82aa0828c673ea3af33e5ef149320a6b6d8f
Jeff Seaman <jeffreyjseaman@gmail.com>
2023-01-24 17:51:49 -0500

    adding new file

---
+++ SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Seaman, Jeffrey on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
